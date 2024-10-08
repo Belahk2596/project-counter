ffe22c61f657723c2690f2bc7ed50542bcf90e3b
Khaleb Ikinepule <khaleb.ikinepule@hotmail.com>
2024-10-08 15:01:01 +1300

    last commit

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
-const myButton = document.getElementById('myButton'); //pulling button into js
-const incrementElmnt = document.getElementById('increment'); //to use for 
-const clickedOnce = document.getElementById('oneClick');
-myButton.addEventListener('click', function() {
-    let increment = parseInt(incrementElmnt.innerHTML);
-   increment++;
-   incrementElmnt.innerHTML = increment; //increments the contents of the element in html
-    if (increment === 1) {
-        clickedOnce.innerHTML = "time" //correcting the grammar used if only clicked once
-    } else {
-        clickedOnce.innerHTML = "times"
-    }
-})
+const myButton = document.getElementById("myButton"); //pulling button into js
+const incrementElmnt = document.getElementById("increment");
+const clickedOnce = document.getElementById("oneClick");
+myButton.addEventListener("click", function () {
+  let increment = parseInt(incrementElmnt.innerHTML); //the value of the inner text
+  increment++; //increments counter
+  incrementElmnt.innerHTML = increment; //prints updated click amount
+  if (increment === 1) {
+    clickedOnce.innerHTML = "time"; //correcting the grammar used if only clicked once
+  } else {
+    clickedOnce.innerHTML = "times";
+  }
+});
