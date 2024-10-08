bacfd002c3b2c7a9a46a3c082dcf0a70561eaf47
Khaleb Ikinepule <khaleb.ikinepule@hotmail.com>
2024-10-08 14:34:32 +1300

    seventh step

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
-const myButton = document.getElementById('myButton');
-const increment = document.getElementById('increment');
-
+const myButton = document.getElementById('myButton'); //pulling button into js
+const incrementElmnt = document.getElementById('increment'); //to use for 
+const clickedOnce = document.getElementById('oneClick');
 myButton.addEventListener('click', function() {
-   increment.innerHTML++;
+    let increment = parseInt(incrementElmnt.innerHTML);
+   increment++;
+   incrementElmnt.innerHTML = increment; //increments the contents of the element in html
+    if (increment === 1) {
+        clickedOnce.innerHTML = "time" //correcting the grammar used if only clicked once
+    } else {
+        clickedOnce.innerHTML = "times"
+    }
 })
