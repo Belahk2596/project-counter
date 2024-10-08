62fd17f5982653f1067be67cd10187c3debffa9b
Khaleb Ikinepule <khaleb.ikinepule@hotmail.com>
2024-10-08 13:54:33 +1300

    fifth step

---
+++ script.js
@@ -0,0 +1,6 @@
+const myButton = document.getElementById('myButton');
+const increment = document.getElementById('increment');
+
+myButton.addEventListener('click', function() {
+    alert('You clicked me');
+})
